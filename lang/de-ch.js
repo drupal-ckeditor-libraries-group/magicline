a428b1c741c1f199b3e54360bb7e18bf637ba431
Wiktor Walc <w.walc@cksource.com>
2016-02-04 11:58:54 +0100

    Updated language files. Added German (Switzerland) localization.

--- lang/de-ch.js
+++ lang/de-ch.js
@@ -3,6 +3,6 @@
  * For licensing, see LICENSE.md or http://ckeditor.com/license
  */
 
-CKEDITOR.plugins.setLang( 'magicline', 'sk', {
-	title: 'Odsek vložiť sem'
+CKEDITOR.plugins.setLang( 'magicline', 'de-ch', {
+	title: 'Absatz hier einfügen'
 } );
